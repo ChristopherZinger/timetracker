1d5071a90271f338033477df9fa61bf83172924a
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-05-28 20:16:02 +0200

    get active categories api call

--- components/apiHooks/getActiveCategories.ts
+++ components/apiHooks/getActiveCategories.ts
@@ -1,33 +1,31 @@
-import { useEffect, useState } from 'react'
-import type { TCategory } from '../../types/domains/Category'
-
-// todo: Remove after firebase call is implemented
-const categories: TCategory[] = [
-  {
-    abbreviation: 'FT',
-    name: 'Free Time',
-    id: '2',
-    isActive: true
-  }
-  ,
-  {
-    abbreviation: 'CR',
-    name: 'Core Responsibility',
-    id: '1',
-    isActive: true
-  }
-]
+import { useContext, useEffect, useState } from 'react'
+import { Category, TCategory } from '../../types/domains/Category'
+import { AppError } from '../../utils/appError'
+import { UserContext } from '../UserContext'
 
 export default function useGetActiveCategories () {
   const [data, setCategories] = useState<undefined | TCategory[]>(undefined)
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState<undefined | Error>(undefined)
+  const { user } = useContext(UserContext)
+
+  const getCategories = async () => {
+    if (!user) {
+      throw new AppError('user_required_to_query_categories')
+    }
+    const category = new Category(user?.uid)
+    return await category.getAllActive()
+  }
 
   useEffect(() => {
     setIsLoading(true)
-    // todo cal firebase api
-    setCategories(categories)
-    setIsLoading(false)
+    getCategories().then(categories => {
+      setCategories(categories)
+      setIsLoading(false)
+    }).catch(err => {
+      setError(err)
+      setIsLoading(false)
+    })
   }, [])
 
   return { isLoading, data, error }
