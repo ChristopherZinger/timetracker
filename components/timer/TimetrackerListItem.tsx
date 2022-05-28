c72c82f5471e571310252e9caee62b42e7a2a2b6
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-05-28 14:57:06 +0200

    add more data to TimetrackerListItem

--- components/timer/TimetrackerListItem.tsx
+++ components/timer/TimetrackerListItem.tsx
@@ -1,14 +1,23 @@
-import { TTrackerInput } from '../../types/domains/Timetracker'
+import type { TCategory } from '../../types/domains/Category'
+import type { TTrackerInput } from '../../types/domains/Timetracker'
+import { TimeUtils } from '../../types/utils/time'
 
 type Props = {
 	item: TTrackerInput
+	categories: TCategory[]
 }
 
-export default function TimetrackerListItem({ item }: Props) {
+export default function TimetrackerListItem({ item, categories }: Props) {
+	const formatTime = TimeUtils.timestampToHourMinute
+
 	return (
 		<div>
-			<span>{item.category}</span>
-			<span>{item.info}</span>
+			<span>{formatTime(item.start)}</span> -
+			<span>{formatTime(item.end)}</span>|{' '}
+			<span>
+				{categories.find((c) => c.id === item.category)?.abbreviation}
+			</span>{' '}
+			| <span>{item.info}</span>
 		</div>
 	)
 }
