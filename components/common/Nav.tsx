4d578b43d60dfbb9b68c68be44c2607bbf04ca14
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-05-29 23:01:15 +0200

    navbar styles

--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -7,13 +7,24 @@ const Nav = () => {
 	const { user } = useContext(UserContext)
 
 	return (
-		<nav>
-			<ul>
-				{user && (
+		<nav className='flex justify-between px-12 py-12 shadow-md shadow-gray-100'>
+			<ul className='flex gap-x-8'>
+				{user ? (
+					<li>
+						<Link href={'/timetracker'}>home</Link>
+					</li>
+				) : (
 					<>
 						<li>
-							<Link href={'/timetracker'}>timetracker</Link>
+							<Link href={'/'}>home</Link>
 						</li>
+					</>
+				)}
+			</ul>
+
+			<ul className='flex gap-x-8'>
+				{user ? (
+					<>
 						<li>
 							<Link href={'/categories'}>categories</Link>
 						</li>
@@ -27,16 +38,10 @@ const Nav = () => {
 							</span>
 						</li>
 					</>
-				)}
-				{!user && (
-					<>
-						<li>
-							<Link href={'/'}>home</Link>
-						</li>
-						<li>
-							<Link href={'/login'}>login</Link>
-						</li>
-					</>
+				) : (
+					<li>
+						<Link href={'/login'}>login</Link>
+					</li>
 				)}
 			</ul>
 		</nav>
