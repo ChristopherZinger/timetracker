53ab25b7146e330228edf2400d05a039e772987a
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-06-05 14:13:23 +0200

    dashboard page wip

---
+++ pages/dashboard.tsx
@@ -0,0 +1,24 @@
+import { ReactElement, useContext } from "react";
+import LoadingBox from "../components/common/LoadingBox";
+import Nav from "../components/common/Nav";
+import Daily from "../components/dashboard/Daily";
+import { UserContext, UserProvider } from "../components/UserContext";
+
+export default function Dashboard() {
+  const { user } = useContext(UserContext);
+
+  if (!user) {
+    return <LoadingBox />;
+  }
+
+  return (
+    <>
+      <Nav />
+      <Daily userId={user.uid} />
+    </>
+  );
+}
+
+Dashboard.getLayout = function getLayout(page: ReactElement) {
+  return <UserProvider>{page}</UserProvider>;
+};
