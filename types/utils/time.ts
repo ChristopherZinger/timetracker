cb75bd92fb20598b8f5b77ad81bc21d1ce0644ca
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-06-05 14:11:20 +0200

    correct how timeutils consume dayjs lib

--- types/utils/time.ts
+++ types/utils/time.ts
@@ -1,9 +1,30 @@
-import dayjs from 'dayjs'
+import day from 'dayjs'
 
+export type Timeframe = {
+  start: number
+  end: number
+}
 export class TimeUtils {
-  constructor() { }
+  private dayjs: (date?: day.ConfigType) => day.Dayjs
+  constructor(d: (date?: day.ConfigType) => day.Dayjs = day) {
+    this.dayjs = d
+  }
 
   public timestampToHourMinute (timestamp: number) {
-    return dayjs(timestamp).format("HH:mm")
+    return this.dayjs(timestamp).format('HH:mm')
+  }
+
+  public getDayTimeframeForDate (date: Date): Timeframe {
+    const dayStart = new Date(date.toDateString())
+    const dayEnd = new Date(dayStart)
+    dayEnd.setDate(dayEnd.getDate() + 1)
+    return {
+      start: dayStart.getTime(),
+      end: dayEnd.getTime()
+    }
+  }
+
+  public timeframeToNrOfMinutes ({ start, end }: Timeframe): number {
+    return Math.abs(this.dayjs(start).diff(end, 'minute'))
   }
 }
